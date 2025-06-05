cb21722d49ec084bd4e0169fdd0940102b21a1e4
gw67-alt <georgewagenknecht1@gmail.com>
2025-06-05 07:14:18 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -226,7 +226,7 @@ while True:
         # Find largest contour
         largest_contour = max(contours, key=cv2.contourArea)
         
-        if len(largest_contour) >= 20:
+        if len(largest_contour) >= 10:
             # Calculate curvature
             curv = curvature(largest_contour, k=15)
             curv_smooth = moving_average(curv, window_size=9)
@@ -236,7 +236,7 @@ while True:
             curv_time_smooth = smoother.get_smoothed()
             
             # Update oscilloscope with smoothed data
-            oscilloscope.add_waveform(curv_time_smooth)
+            oscilloscope.add_waveform(abs(-1/curv_time_smooth))
             
             # Draw contour on frame
             cv2.drawContours(display_frame, [largest_contour], -1, (0, 255, 0), 2)
